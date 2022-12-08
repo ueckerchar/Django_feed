a9b048f09331148ff5a9209125a46f673de9eb4d
Charlie <Charles_Uecker1@baylor.edu>
2022-12-07 22:51:40 -0600

    3/5, need questions answered

--- FeedApp/urls.py
+++ FeedApp/urls.py
@@ -5,7 +5,13 @@ app_name = 'FeedApp'
 
 urlpatterns = [
     path('', views.index, name='index'),
-    
+    path('profile/', views.profile, name='profile'),
+    path('myfeed', views.myfeed, name='myfeed'),
+    path('friendsfeed', views.friendsfeed, name='friendsfeed'),
+    path('new_post/', views.new_post, name='new_post'),
+    path('comments/<int:post_id>/', views.comments, name='comments'),
+    path('friends/', views.friends, name='friends'),
+
     ]
 
     
